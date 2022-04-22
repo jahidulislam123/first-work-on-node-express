0b69f8f44eb1c1a17365cf97653276017237f518
unknown <islam15-3879@diu.edu.bd>
2022-04-22 17:16:51 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port =process.env.PORT||5000;
+app.use(cors())
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello i am hi jahid vai  from my over smart  World!')
@@ -20,6 +23,14 @@ const users =[
 
 app.get('/users',(req,res)=>{
     // res.send({id: 1 ,name: 'abdul alim',job : 'khay sudu halim'});
+        if(req.query.name){
+            const search =req.query.name.toLocaleLowerCase();
+            const matched=users.filter(user=> user.name.toLocaleLowerCase().includes(search))
+            res.send(matched);
+        }
+        else{
+
+        }
     res.send(users);
 });
 app.get('/user/:id',(req,res)=>{
@@ -35,7 +46,17 @@ app.get('/user/:id',(req,res)=>{
 
 
 })
+app.get('/fruits/mango/fazle',(req,res)=>{
+    res.send('sawar fazle favour');
+})
 
+app.post('/user',(req,res)=>{
+    console.log('request',req.body);
+    const user=req.body;
+    user.id=users.length+1;
+    users.push(user);
+    res.send(user);
+})
 app.get('/fruits',(req,res)=>{
     res.send(["mang",'apple','orange']);
 })
